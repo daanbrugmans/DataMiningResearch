a4d4db2bd5949e5d26e4cdbed21ed39650d1c4da
Daan Brugmans <daanbrugmans@gmail.com>
2021-11-18 14:42:15 +0100

    Added import code for CSV

--- Preprocessing.R
+++ Preprocessing.R
@@ -4,3 +4,9 @@ library(ggplot2)
 library(dplyr)
 library(caret)
 
+#Change working directory if needed, Josse.
+setwd("C:/Users/Daan/Documents/Projecten/DataMiningResearch")
+
+bfrss.df <- read.csv("BRFSS2015.csv", header = T)
+
+
