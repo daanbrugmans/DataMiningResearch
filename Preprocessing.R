acb85004351b751fd48658135c4a1e1ae2c0a1c1
Daan Brugmans <daanbrugmans@gmail.com>
2021-11-26 12:04:18 +0100

    Added conversion for BMI from continuous to categorical

--- Preprocessing.R
+++ Preprocessing.R
@@ -66,16 +66,44 @@ summary(brfss.df)
     # 1. Underweight (BMI < 18.5)
     # 2. Healthy weight (18.5 <= BMI < 25)
     # 3. Overweight (25 <= BMI < 30)
-    # 4. Obese (BMI => 30)
-  # Categories taken from Voedingscentrum (https://www.voedingscentrum.nl/nl/bmi-meter.aspx)
+    # 4. Class 1 Obese (30 <= BMI < 35)
+    # 5. Class 2 Obese (35 <= BMI < 40)
+    # 6. Class 3 Obese (BMI >= 40)
+  # Categories taken from CDC (https://www.cdc.gov/obesity/adult/defining.html)
+BMI.to.categorical <- function(BMI.value){
+  if (BMI.value >= 40) {
+    BMI.value <- "Class 3 Obese"
+  } else if (BMI.value >= 35) {
+    BMI.value <- "Class 2 Obese"
+  } else if (BMI.value >= 30) {
+    BMI.value <- "Class 1 Obese"
+  } else if (BMI.value >= 25) {
+    BMI.value <- "Overweight"
+  } else if (BMI.value >= 18.5) {
+    BMI.value <- "Healthy weight"
+  } else {
+    BMI.value <- "Underweight"
+  }
+}
+
+brfss.df$BMI <- sapply(brfss.df$BMI, BMI.to.categorical)
 
 
+# Give the following features more readable names: 
+  
 
 # Bar plots
 ggplot(brfss.df, aes(x=HeartDiseaseorAttack)) + 
   geom_bar()
 
 ggplot(brfss.df, aes(x=HighBP, fill=HeartDiseaseorAttack)) + 
+  labs(fill="Has had Heart Disease\nor Heart Attack") +
+  theme(legend.position="right") +
+  scale_fill_grey() +
+  geom_bar()
+
+BMI.level.order <- c("Underweight", "Healthy weight", "Overweight", "Class 1 Obese", "Class 2 Obese", "Class 3 Obese")
+ggplot(brfss.df, aes(x=factor(BMI, levels=BMI.level.order), fill=HeartDiseaseorAttack)) + 
   labs(fill="Has had Heart Disease\nor Heart Attack") +
   theme(legend.position="right") +
   scale_fill_grey() +
